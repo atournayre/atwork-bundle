8fa20fff6650f6cfe979ca7f9a481c160cf814c4
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-04-10 17:25:19 +0200

    feat(type): EmailAddress

---
+++ Type/EmailAddress.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Atournayre\Bundle\AtWorkBundle\Type;
+
+use Webmozart\Assert\Assert;
+
+readonly class EmailAddress
+{
+    public ?string $email;
+
+    private function __construct(string $email)
+    {
+        Assert::email($email);
+
+        $this->email = $email;
+    }
+
+    public static function fromString(string $email): self
+    {
+        return new static($email);
+    }
+}
