6b99eb2cbde2dc51c571503d42c0f99a2623897b
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-04-10 23:33:20 +0200

    fix(dependency injection): Inject specific parameter instead of ParameterBagInterface.

--- Fixture/Provider/HashPasswordProvider.php
+++ Fixture/Provider/HashPasswordProvider.php
@@ -4,22 +4,22 @@ namespace Atournayre\Bundle\AtWorkBundle\Fixture\Provider;
 
 use Atournayre\Bundle\AtWorkBundle\Service\Security\PasswordHasherService;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class HashPasswordProvider
 {
+    private string $userClass;
+
     public function __construct(
         private readonly PasswordHasherService $passwordHasherService,
-        private readonly ParameterBagInterface $parameterBag,
+        #[Autowire('atournayre_atwork.security.providers.entity.class')] string $userClass,
     )
     {
+        $this->userClass = $userClass;
     }
 
     public function hashPassword(string $plainPassword): string
     {
-        $userClass = $this->parameterBag->get('atournayre_atwork.security.providers.entity.class');
-
-        $user = new $userClass();
+        $user = new ($this->userClass)();
         return $this->passwordHasherService->generate($user, $plainPassword);
     }
 }
