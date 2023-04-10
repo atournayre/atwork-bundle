c9414acb5be4d972eba6f1282e0d3600d3d46f3f
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-04-10 23:21:18 +0200

    fix(dependency injection): Inject configuration to get user entity class for HashPasswordProvider.

--- Fixture/Provider/HashPasswordProvider.php
+++ Fixture/Provider/HashPasswordProvider.php
@@ -3,17 +3,23 @@
 namespace Atournayre\Bundle\AtWorkBundle\Fixture\Provider;
 
 use Atournayre\Bundle\AtWorkBundle\Service\Security\PasswordHasherService;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class HashPasswordProvider
 {
     public function __construct(
         private readonly PasswordHasherService $passwordHasherService,
+        private readonly ParameterBagInterface $parameterBag,
     )
     {
     }
 
     public function hashPassword(string $plainPassword): string
     {
-        return $this->passwordHasherService->generate('', $plainPassword);
+        $userClass = $this->parameterBag->get('atournayre_atwork.security.providers.entity.class');
+
+        $user = new $userClass();
+        return $this->passwordHasherService->generate($user, $plainPassword);
     }
 }
