8d984892853e47418dd74325e13b8c01bd89badd
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-04-10 17:07:13 +0200

    feat(security): Add SecurityJsonControllerTrait

---
+++ Trait/Controller/ApiPlatform/SecurityJsonControllerTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Atournayre\Bundle\AtworkBundle\Trait\Controller\ApiPlatform;
+
+use ApiPlatform\Api\IriConverterInterface;
+use ApiPlatform\Api\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+trait SecurityJsonControllerTrait
+{
+    #[Route('/login', name: 'app_login', methods: [Request::METHOD_POST])]
+    public function login(
+        IriConverterInterface $iriConverter,
+        #[CurrentUser] UserInterface $user = null
+    ): Response {
+        if (!$user) {
+            return $this->json([
+                'error' => 'Connexion request is invalid.',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return new Response(null, Response::HTTP_NO_CONTENT, [
+            'Location' => $iriConverter->getIriFromResource($user, UrlGeneratorInterface::ABS_URL),
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
