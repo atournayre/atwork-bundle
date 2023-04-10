e29534cbfd33cdf83259b579ff5b4672eb3cc549
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2023-04-10 18:55:19 +0200

    feat(dependency injection): Add declaration for OverrideMakeEntityCommand

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -2,14 +2,18 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Atournayre\Bundle\AtWorkBundle\Command\OverrideMakeEntityCommand;
 use Atournayre\Bundle\AtWorkBundle\Contracts\DoctrineType;
 use Atournayre\Bundle\AtWorkBundle\Contracts\FixtureProvider;
 use Atournayre\Bundle\AtWorkBundle\DependencyInjection\CompilerPass\DoctrineTypePass;
 
 return static function (ContainerConfigurator $container) {
-    $container->services()
+    $services = $container->services()
         ->defaults()
         ->instanceof(FixtureProvider::class)->tag('nelmio_alice.fixture_provider')
         // TODO Replace by recipe with all the types commented
         ->instanceof(DoctrineType::class)->tag(DoctrineTypePass::TAG);
+
+    $services
+        ->set(OverrideMakeEntityCommand::class)->public()->tag('console.command');
 };
